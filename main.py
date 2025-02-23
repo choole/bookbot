653c194b16675070a723ccf69b0aebb9eb3f3606
choole <nemanjacukic31@gmail.com>
2025-02-23 14:24:05 +0100

    update: path to book as argv

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import get_num_words, get_num_letters, sort_dictionary
+from sys import argv, exit
 
 
 def get_book_text(filePath):
@@ -9,16 +10,20 @@ def get_book_text(filePath):
 
 
 def main():
-    print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
-    words = get_book_text("./books/frankenstein.txt")
-    dictionary = get_num_letters(words)
-    print("--------- Character Count -------")
-    sortedList = sort_dictionary(dictionary)
-    for i in sortedList:
-        if (next(iter(i)).isalpha()):
-            print(f"{next(iter(i))}: {i[next(iter(i))]}")
-    print("============= END ===============")
+    if (len(argv) < 2):
+        print("Usage: python3 main.py <path_to_book>")
+        exit(1)
+    else:
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at {argv[1]}...")
+        words = get_book_text(argv[1])
+        dictionary = get_num_letters(words)
+        print("--------- Character Count -------")
+        sortedList = sort_dictionary(dictionary)
+        for i in sortedList:
+            if (next(iter(i)).isalpha()):
+                print(f"{next(iter(i))}: {i[next(iter(i))]}")
+        print("============= END ===============")
 
 
 main()
