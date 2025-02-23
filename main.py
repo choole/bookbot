a5b0c74f12ccdc82769f26467d763c99fd4b2ae6
choole <nemanjacukic31@gmail.com>
2025-02-23 14:16:08 +0100

    finish dictionary

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
-from stats import get_num_words
+from stats import get_num_words, get_num_letters, sort_dictionary
 
 
 def get_book_text(filePath):
     with open(filePath) as f:
         file_contents = f.read()
         # print(file_contents)
-        get_num_words(file_contents)
+        return get_num_words(file_contents)
 
 
 def main():
-    get_book_text("./books/frankenstein.txt")
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    words = get_book_text("./books/frankenstein.txt")
+    dictionary = get_num_letters(words)
+    print("--------- Character Count -------")
+    sortedList = sort_dictionary(dictionary)
+    for i in sortedList:
+        if (next(iter(i)).isalpha()):
+            print(f"{next(iter(i))}: {i[next(iter(i))]}")
+    print("============= END ===============")
 
 
 main()
-
--- stats.py
+++ stats.py
@@ -1,3 +1,21 @@
 def get_num_words(bookText):
     words = bookText.split()
-    print(f"{len(words)} words found in the document")
+    print("----------- Word Count ----------")
+    print(f"Found {len(words)} total words")
+    return words
+
+
+def get_num_letters(words):
+    letters = []
+    for x in words:
+        letters.append(list(x.lower()))
+    single_list = [item for sublist in letters for item in sublist]
+    char_counts = {char: single_list.count(char) for char in set(single_list)}
+    return char_counts
+
+
+def sort_dictionary(dictionary):
+    # sorted = []
+    char_list = [{char: count} for char, count in dictionary.items()]
+    char_list.sort(key=lambda d: list(d.values())[0], reverse=True)
+    return char_list
